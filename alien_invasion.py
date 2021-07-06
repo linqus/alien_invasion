14d12668c267c3ee02cbe028e35880236030b19a
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-06 19:32:16 +0200

    zakończenie gry

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,26 +42,31 @@ class AlienInvasion:
 		""" Start the main loop for the game. """
 		while True:
 			self._check_events()
-			self.ship.update()
-			self._update_bullets()
-			self._update_aliens()
+			if self.stats.game_active:
+				self.ship.update()
+				self._update_bullets()
+				self._update_aliens()
 			self._update_screen()
 
 	def _ship_hit(self):
 		""" Repond to the ship being hit by an alien """
-		# Decrement ships left
-		self.stats.ships_left -= 1
+		if self.stats.ships_left > 0:
 
-		# Get rid of any remaining aliens and bullets
-		self.aliens.empty()
-		self.bullets.empty()
+			# Decrement ships left
+			self.stats.ships_left -= 1
+			print(self.stats.ships_left)
+			# Get rid of any remaining aliens and bullets
+			self.aliens.empty()
+			self.bullets.empty()
 
-		# Create new sheep and fleet
-		self._create_fleet()
-		self.ship.center_ship()
+			# Create new sheep and fleet
+			self._create_fleet()
+			self.ship.center_ship()
 
-		# Pause
-		sleep(0.5)
+			# Pause
+			sleep(0.5)
+		else:
+			self.stats.game_active = False
 
 
 	def _check_events(self):
